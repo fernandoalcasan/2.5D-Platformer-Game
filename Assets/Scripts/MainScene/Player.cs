5161614deaa0db67a82106891c5b30e205848214
fernandoalcasan <fernandoalcasan@outlook.com>
2021-08-17 15:01:38 -0500

    Character animations to idel, walk, run, jump and jump running implemented

--- Assets/Scripts/MainScene/Player.cs
+++ Assets/Scripts/MainScene/Player.cs
@@ -13,30 +13,71 @@ public class Player : MonoBehaviour
     private float _gravity;
     private float _gravityImpulse;
     private Vector3 _movement;
+    private Vector3 _facing;
     private CharacterController _player;
+    private Animator _anim;
+    private bool _running;
+    private bool _jumping;
 
     void Start()
     {
         _player = GetComponent<CharacterController>();
         if (_player is null)
             Debug.LogError("Character Controller is NULL");
+        
+        _anim = GetComponentInChildren<Animator>();
+        if (_anim is null)
+            Debug.LogError("Animator in children is NULL");
     }
 
     void Update()
     {
         _direction = Input.GetAxis("Horizontal");
         _movement = new Vector3(_direction * _speed, 0f, 0f);
+        _anim.SetFloat("Speed", Mathf.Abs(_direction));
+
+        if(_direction != 0f)
+        {
+            _facing = transform.localEulerAngles;
+            _facing.y = _direction > 0f ? 0f : 180f;
+            transform.localEulerAngles = _facing;
+        }
 
         if(_player.isGrounded)
         {
+            if (_jumping)
+            {
+                _jumping = false;
+                _anim.SetBool("Jumping", false);
+            }
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                _movement.x *= 2f;
+                _anim.SetBool("Running", true);
+                _running = true;
+            }
+
             if(Input.GetKeyDown(KeyCode.Space))
             {
-                _gravityImpulse = _jumpPower;
+                if(_running)
+                    _gravityImpulse = _jumpPower * 1.25f;
+                else
+                    _gravityImpulse = _jumpPower;
+
+                _anim.SetBool("Jumping", true);
+                _jumping = true;
             }
         }
         else
         {
             _gravityImpulse -= _gravity * Time.deltaTime;            
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _anim.SetBool("Running", false);
+            _running = false;
         }
 
         _movement.y = _gravityImpulse;
